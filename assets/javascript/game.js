86c771d66d5ee375bd51459c25b760bb325165b3
mwomack <mikewomack2000@yahoo.com>
2018-08-22 18:53:37 -0700

    functionality working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,44 @@
 
 $(document).ready(function () {
 
-
-    
-    //var targetNumber;
+    startGame();
 
     //var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     var buttons = document.getElementsByClassName("crystal");
-    //var buttons = $(".crystal");
-    var currentScore = 0; // players score counter
-    var targetNumber = Math.floor(Math.random() * 101) + 19;
+    var currentScore; // players score counter
+    //var targetNumber = Math.floor(Math.random() * 101) + 19;
+    var targetNumber;
     var wins = 0;
     var losses = 0;
+    var button;
 
-    startGame();
 
     function startGame() {
 
+        //reset current score back to zero
+        currentScore = 0;
+        $("#current-score").text(currentScore);
+
         // set random target number for game between 19 and 120.
-        var targetNumber = Math.floor(Math.random() * 101) + 19;
+        targetNumber = Math.floor(Math.random() * 101) + 19;
         $("#random-number").text(targetNumber);
 
+        // update wins/losses
         $("#win-count").text(wins);
         $("#loss-count").text(losses);
-        currentScore = 0;
 
     };
 
-    // Loop through all of the (crystal) button elements
-    for (let i = 0; i < buttons.length; i++) {
-        //  store current button element into the button variable
-        let button = buttons[i];
-        //  set a custom attribute on the button element to a radom number
-        $(button).attr("data-value", Math.floor(Math.random() * 12) + 1);
-    }
+    
+        // Loop through all of the (crystal) button elements
+        for (let i = 0; i < buttons.length; i++) {
+            // store current button element into the button variable
+            button = buttons[i];
+            //  set a custom attribute on the button element to a radom number
+            $(button).attr("data-value", Math.floor(Math.random() * 12) + 1);
 
-
+        };
+    
 
     $(".crystal").on("click", function () {
 
@@ -47,18 +50,33 @@ $(document).ready(function () {
         $("#current-score").text(currentScore);
 
         if (currentScore === targetNumber) {
-            alert("You win!!")
+            $("#current-score").text(currentScore);
             wins++;
+            alert("You win!!");
             startGame();
+            for (let i = 0; i < buttons.length; i++) {
+                // store current button element into the button variable
+                button = buttons[i];
+                //  set a custom attribute on the button element to a radom number
+                $(button).attr("data-value", Math.floor(Math.random() * 12) + 1);
+    
+            };
         }
 
-        else if (currentScore > targetNumber) {
-            alert("You lose!!")
+        else if (currentScore >= targetNumber) {
+            $("#current-score").text(currentScore);
             losses++;
+            alert("You lose!!");
             startGame();
-        }
+            for (let i = 0; i < buttons.length; i++) {
+                // store current button element into the button variable
+                button = buttons[i];
+                //  set a custom attribute on the button element to a radom number
+                $(button).attr("data-value", Math.floor(Math.random() * 12) + 1);
+    
+            };
+        };
 
-        
     });
 
 });
